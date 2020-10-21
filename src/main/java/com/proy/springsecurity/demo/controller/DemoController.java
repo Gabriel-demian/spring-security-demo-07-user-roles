22d73d15765d7fe8141042603d28aa56a15ecd4c
Gabriel <57782295+Gabriel-demian@users.noreply.github.com>
2020-10-21 18:34:13 -0300

    Initial commit

---
+++ src/main/java/com/proy/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,29 @@
+package com.proy.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	// Add request mapping for home /
+	@GetMapping("/")
+	public String showHome() {
+		
+		return "home";
+	}
+	
+	// Add request mapping for /leaders
+	@GetMapping("/leaders")
+	public String showLeaders() {
+		
+		return "leaders";
+	}
+	
+	// Add request mapping for /systems
+		@GetMapping("/systems")
+		public String showSystems() {
+			
+			return "systems";
+		}
+}
